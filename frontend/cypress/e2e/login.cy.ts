de4c0ee62cdfb4a2146c1727428eb56cec91df20
abos-gergo <gergo.abos@gmail.com>
2025-02-25 23:33:32 +0100

    Login page E2E tests

--- frontend/cypress/e2e/login.cy.ts
+++ frontend/cypress/e2e/login.cy.ts
@@ -4,27 +4,104 @@ describe('Login Page', () => {
   });
 
   it('should display the login form correctly', displayLoginForm);
-  it('should validate required fields', validateRequiredFields);
+  it('should validate password', validatePassword);
+  it('should validate username', validateUsername);
   it('should enable submit button', enableSubmitButton);
-  it('should show/hide password when toggle button is clicked', togglePassword);
+  it(
+    'should show/hide password when toggle button is clicked',
+    toggleShowPassword
+  );
   it('should switch between login and signup prompts', toggleSignIn);
   it('should attempt to submit the form', sendRequest);
 
   function displayLoginForm() {
+    //Elements should exist
     cy.get('#auth-card').should('exist');
     cy.get('input[formControlName="username"]').should('exist');
     cy.get('input[formControlName="password"]').should('exist');
+    cy.get('button[type=submit]').contains('Sign In').should('exist');
     cy.contains("Don't have an account?").should('exist');
   }
 
-  function validateRequiredFields() {
-    cy.get('button[type="submit"]').should('be.disabled');
-
-    cy.get('input[formControlName="username"]').click().blur();
-    cy.contains('Username field is required').should('exist');
-
+  function validatePassword() {
+    // Required validator
     cy.get('input[formControlName="password"]').click().blur();
     cy.contains('Password field is required').should('exist');
+
+    // Error shows up in the mat-error of the input
+    cy.get('input[formControlName="password"]')
+      .parents('mat-form-field')
+      .find('mat-error')
+      .should('exist');
+
+    // MinLength validator
+    cy.get('input[formControlName="password"]').type('short').blur();
+    cy.contains('Password has to be at least 8 characters long').should(
+      'exist'
+    );
+
+    // Custom Password Validator
+    cy.get('input[formControlName="password"]')
+      .clear()
+      .type('PASSWORD1')
+      .blur();
+    cy.contains('Password must contain at least one lowercase letter').should(
+      'exist'
+    );
+
+    // Custom Password Validator
+    cy.get('input[formControlName="password"]')
+      .clear()
+      .type('password1')
+      .blur();
+    cy.contains('Password must contain at least one uppercase letter').should(
+      'exist'
+    );
+
+    // Custom Password Validator
+    cy.get('input[formControlName="password"]').clear().type('Password').blur();
+    cy.contains('Password must contain at least one number').should('exist');
+
+    // Mat-error should not exist if valid password is given
+    cy.get('input[formControlName="password"]')
+      .clear()
+      .type('Password1')
+      .blur();
+    cy.get('input[formControlName="password"]')
+      .parents('mat-form-field')
+      .find('mat-error')
+      .should('not.exist');
+  }
+
+  function validateUsername() {
+    // Required validator
+    cy.get('input[formControlName="username"]').click().blur();
+    cy.contains('Username field is required');
+
+    // Error shows up in the mat-error of the input
+    cy.get('input[formControlName="username"]')
+      .parents('mat-form-field')
+      .find('mat-error')
+      .should('exist');
+
+    // MinLength validator
+    cy.get('input[formControlName="username"]').type('a').blur();
+    cy.contains('Username has to be at least 8 characters long').should(
+      'exist'
+    );
+
+    // Username Pattern (regEx) Validator
+    cy.get('input[formControlName="username"]').clear().type('aaaaaaa@').blur();
+    cy.contains(
+      'Username can only contain letters, numbers, underscores, and dashes'
+    ).should('exist');
+
+    // Mat-error should not exist if valid username is given
+    cy.get('input[formControlName="username"]').clear().type('aaaaaaaa').blur();
+    cy.get('input[formControlName="username"]')
+      .parents('mat-form-field')
+      .find('mat-error')
+      .should('not.exist');
   }
 
   function enableSubmitButton() {
@@ -36,7 +113,7 @@ describe('Login Page', () => {
     cy.get('button[type="submit"]').should('not.be.disabled');
   }
 
-  function togglePassword() {
+  function toggleShowPassword() {
     cy.get('input[formControlName="password"]').should(
       'have.attr',
       'type',
@@ -59,6 +136,7 @@ describe('Login Page', () => {
   }
 
   function toggleSignIn() {
+    cy.contains("Don't have an account?").should('exist');
     cy.contains('Sign In').should('exist');
     cy.contains("Don't have an account?").click();
 
